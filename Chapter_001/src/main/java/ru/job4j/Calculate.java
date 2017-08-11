15448fa3c22f4d9d1f3df3862024d09a0347fe2a
EvgeniyUlanov <komrad1812@yandex.ru>
2017-08-11 19:47:56 +0300

    task 4122 JUnit

--- Chapter_001/src/main/java/ru/job4j/Calculate.java
+++ Chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+ *Class Calculate решение задачи части 001 урок1, создание Hello World/
+ *@autor Evgeniy Ulanov
+ *@since 11.08.2017
+*/
+
+
 public class Calculate {
+
+    /**
+     *метод main выводит в консоль строку "Hello Wolrd"
+    */
     public static void main (String[] args){
     	System.out.println("Hello World");
     }
---
+++ Chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+*Package for calculate Test
+*
+*@autor Evgeiniy Ulanov
+*@version $ld$
+*@since 0.1
+*/
+package ru.job4j;

89bf5785c4551f654e649066f0792bdc248e01e4
EvgeniyUlanov <komrad1812@yandex.ru>
2017-11-01 13:30:39 +0300

    simple commit

--- chapter_004/src/main/java/ru/job4j/testtask/BookParserSax.java
+++ chapter_004/src/main/java/ru/job4j/testtask/BookParserSax.java
@@ -9,6 +9,7 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 
---
+++ chapter_007/src/main/java/ru/job4j/jmm/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for jmm.
+ *
+ * @author Evgeiniy Ulanov.
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.jmm;
